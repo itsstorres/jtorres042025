6aa7f50b6e708bd841467c576854ad736565493d
Jonathan Torres <jtorr-s837@chusd.org>
2025-11-14 11:42:05 -0800

    Inital Commit

---
+++ Demos/VSCodeExtensions/scripts.js
@@ -0,0 +1,12 @@
+// This is a JavaScript Comment
+// ? This is a question comment
+// ! This is an Alert Comment
+// todo This is a TODO comment
+// * This is another regular comment
+
+
+
+
+
+
+window.alert("Hello!")
---
+++ README.md
@@ -0,0 +1,3 @@
+# JonathanTorres
+
+## Portfolio
